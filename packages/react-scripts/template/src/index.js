d987ab8a827cd6e9a0ecdbb2808e4b6858dfa81c
Joel Detweiler <joeld@ifit.com>
2018-03-13 18:34:52 -0400

    Changing the template project

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.scss';
+import reducers from './reducers';
+import * as serviceWorker from './serviceWorker';
+
+const middleWareStore = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={middleWareStore(reducers)}>
+    <div>Quickstart-React</div>
+  </Provider>,
+  document.getElementById('root')
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ packages/react-scripts/template/src/reducers.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+/* new imports go here - do not remove or change this line */
+
+const rootReducer = combineReducers(
+  {
+    /* new reducers go here - do not remove or change this line */
+  }
+);
+
+export default rootReducer;
