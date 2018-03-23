ce4b6bb1cd6abc92624f693c322807b514dbae45
Joel Detweiler <joeld@ifit.com>
2018-03-23 13:13:58 -0400

    Add back raf polyfill for testing

--- packages/react-scripts/config/polyfills.js
+++ packages/react-scripts/config/polyfills.js
@@ -9,3 +9,9 @@
 'use strict';
 
 require('babel-polyfill');
+
+// In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
+// We don't polyfill it in the browser--this is user's responsibility.
+if (process.env.NODE_ENV === 'test') {
+  require('raf').polyfill(global);
+}
